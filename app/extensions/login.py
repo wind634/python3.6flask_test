389ff9648dcbeb8fba5950f1249acadf67577a67
wind634 <2911184332@qq.com>
2017-03-30 14:20:15 +0800

    初始化提交

--- app/extensions/login.py
+++ app/extensions/login.py
@@ -2,8 +2,11 @@ from flask_login import LoginManager
 
 login_manager = LoginManager()
 
+# 登录视图的名称
 login_manager.login_view = 'auth.login'
+# 没有登陆情况下默认的闪现消息
 login_manager.login_message = u"你没有登录，请先登陆"
+# 自定义消息分类
 login_manager.login_message_category = "error"
 
 
