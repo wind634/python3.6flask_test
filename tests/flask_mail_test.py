44bddb3b687c66855f420af0ba4871df267a00c9
wind634 <2911184332@qq.com>
2017-04-05 15:23:09 +0800

    新增邮件测试

--- tests/flask_mail_test.py
+++ tests/flask_mail_test.py
@@ -3,6 +3,8 @@ from flask_mail import Mail
 
 # app = Flask(__name__)
 from app import create_app
+from threading import Thread
+
 
 app = create_app('default')
 mail = Mail(app)
@@ -10,12 +12,22 @@ mail = Mail(app)
 from flask_mail import Message
 
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+        
+
 @app.route("/")
 def index():
-    msg = Message("Hello",
-                  sender="2911184332@qq.com",
+    msg = Message("关于重要事情的商讨通知",
                   recipients=["2911184332@qq.com"])
-    mail.send(msg)
+    # with app.open_resource("../tests/test.jpeg") as fp:
+    #     msg.attach("test.jpeg", "image/jpeg", fp.read())
+    # msg.body = "我爱你"
+    msg.html = "<b>我爱你</b>"
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    # mail.send(msg)
     return "hello"
 
 app.run(debug=True)
