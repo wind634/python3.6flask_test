66b492e08abc7eed54808cf22f6fb9b21918244e
wind634 <2911184332@qq.com>
2017-04-05 15:50:22 +0800

    新增邮件测试

---
+++ tests/flask_cache_test.py
@@ -0,0 +1,30 @@
+import random
+
+from flask import Flask
+from flask_mail import Mail
+
+# app = Flask(__name__)
+from app import create_app
+from app.extensions.cache import cache
+
+app = create_app('default')
+mail = Mail(app)
+
+
+with app.app_context():
+    @cache.memoize(timeout=50)
+    def big_foo(a, b):
+        return a + b + random.randrange(0, 1000)
+    print(big_foo(5, 2))
+    print(big_foo(5, 3))
+    cache.delete_memoized('big_foo')
+    print(big_foo(5, 2))
+
+
+# @app.route("/")
+# @cache.cached(timeout=50)
+# def index():
+#
+#     return "hello"
+#
+# app.run(debug=True)
