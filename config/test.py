53896bacb1b796d828e04fc362f28d303ac81133
wind634 <2911184332@qq.com>
2017-04-07 09:51:13 +0800

    新增flask-admin

--- config/test.py
+++ config/test.py
@@ -1,5 +1,7 @@
 import os
 
+from flask_uploads import IMAGES, DOCUMENTS
+
 from config.common import Config, basedir
 
 
@@ -7,3 +9,11 @@ class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+    # ===== 上传相关配置 start =====
+    UPLOADS_DEFAULT_DEST = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "uploads")
+    UPLOADED_PHOTO_DEST = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "uploads", "photos")
+    UPLOADED_PHOTO_ALLOW = IMAGES
+    UPLOADED_PHOTO_DENY = DOCUMENTS
+    # ===== 上传相关配置 end =====
